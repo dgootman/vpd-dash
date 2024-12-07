0bba3cfd0d36a6deecc012de03d4f018da55a36a
David Gootman <gootman@gmail.com>
2024-12-07 05:12:50 +0000

    More graphs and filters

--- vpd_dash.py
+++ vpd_dash.py
@@ -8,11 +8,11 @@ import streamlit as st
 from colorhash import ColorHash
 from pyproj import Proj
 
+url = "https://geodash.vpd.ca/opendata/crimedata_download/AllNeighbourhoods_AllYears/crimedata_csv_AllNeighbourhoods_AllYears.zip"
+
 
 @st.cache_data
 def load_data():
-    url = "https://geodash.vpd.ca/opendata/crimedata_download/AllNeighbourhoods_AllYears/crimedata_csv_AllNeighbourhoods_AllYears.zip"
-
     with (
         requests.get(url) as r,
         ZipFile(BytesIO(r.content)) as z,
@@ -22,13 +22,10 @@ def load_data():
 
 
 def main():
-    st.title("Vancouver Crime Data")
+    st.set_page_config("Vancouver Crime Data", layout="wide")
+    st.title(f"Vancouver Crime Data [:material/download:]({url})")
 
     data = load_data()
-
-    if st.checkbox("Show raw data"):
-        st.subheader("Raw data")
-        st.dataframe(data)
 
     data["date"] = pd.to_datetime(data[["YEAR", "MONTH", "DAY", "HOUR", "MINUTE"]])
 
@@ -38,30 +35,62 @@ def main():
     data.Y = data.Y.replace(0, None)
     data["lon"], data["lat"] = p(data.X, data.Y, inverse=True, errcheck=True)
 
-    year_range = st.slider(
-        "Year range",
+    col1, col2, col3 = st.columns(3)
+
+    neighbourhoods = col1.multiselect(
+        "Neighbourhoods", options=sorted(data.NEIGHBOURHOOD.dropna().unique())
+    )
+
+    crime_types = col2.multiselect("Types of crimes", data.TYPE.unique())
+
+    year_range = col3.slider(
+        "Date range",
         data.YEAR.min(),
         data.YEAR.max(),
         (data.YEAR.min(), data.YEAR.max()),
     )
 
-    crime_types = st.multiselect("Types of crimes", data.TYPE.unique())
-
     data = data[
         data.YEAR.between(*year_range)
+        & ((not neighbourhoods) | (data.NEIGHBOURHOOD.isin(neighbourhoods)))
         & ((not crime_types) | (data.TYPE.isin(crime_types)))
     ]
 
-    st.subheader("Crimes by hour")
-    st.bar_chart(data.HOUR.value_counts())
+    st.subheader("Crimes by year")
+    st.plotly_chart(
+        px.line(
+            data.YEAR.value_counts().reset_index().sort_values("YEAR"),
+            x="YEAR",
+            y="count",
+        )
+    )
 
-    st.subheader("Crimes by year and type")
+    st.subheader("Crimes by type")
     st.plotly_chart(
         px.line(
             data[["YEAR", "TYPE"]].value_counts().reset_index().sort_values("YEAR"),
             x="YEAR",
             y="count",
             color="TYPE",
+            category_orders={"TYPE": sorted(data.TYPE.dropna().unique())},
+            height=800,
+        )
+    )
+
+    st.subheader("Crimes by neighbourhood")
+    st.plotly_chart(
+        px.line(
+            data[["YEAR", "NEIGHBOURHOOD"]]
+            .value_counts()
+            .reset_index()
+            .sort_values("YEAR"),
+            x="YEAR",
+            y="count",
+            color="NEIGHBOURHOOD",
+            category_orders={
+                "NEIGHBOURHOOD": sorted(data.NEIGHBOURHOOD.dropna().unique())
+            },
+            height=800,
         )
     )
 
@@ -71,7 +100,7 @@ def main():
     st.subheader("Crimes by type")
     type_data = data.TYPE.value_counts().to_frame()
     type_data["color"] = type_data.index.map(COLOR_MAP)
-    st.bar_chart(type_data, y="count", color="color")
+    st.bar_chart(type_data, horizontal=True, y="count", color="color")
 
     map_data = data[data["lat"].notna() & data["lon"].notna()]
     map_data["color"] = map_data[COLOR_COLUMN].map(COLOR_MAP)
@@ -81,6 +110,7 @@ def main():
         map_data.value_counts(["lat", "lon", "color"]).to_frame().reset_index(),
         size="count",
         color="color",
+        height=800,
     )
 
 
